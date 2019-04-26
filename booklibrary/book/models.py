40b9c5aa19469387832e232569da821266b65ee1
wangshuo <670681219@qq.com>
2019-04-26 15:45:46 +0800

    创建类生成数据库

---
+++ booklibrary/book/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Suser(User):
+    college = models.CharField(max_length=30,null=True,blank=True)
+    uno = models.IntegerField(null=True,blank=True)
+
+class Book(models.Model):
+    bname = models.CharField(max_length=255)
+    auther = models.CharField(max_length=20)
+    public_com = models.CharField(max_length=255,null=True,blank=True)
+    public_date = models.CharField(max_length=30,null=True,blank=True)
+
+    def __str__(self):
+        return self.bname
+
+class Borrows(models.Model):
+    uname = models.ForeignKey(User,on_delete=models.CASCADE)
+    bname = models.ForeignKey(Book,on_delete=models.CASCADE)
+    date_borrow = models.DateTimeField(auto_now_add=True)
+    date_retur = models.DateTimeField()
+    status = models.BooleanField(default=False)
---
+++ booklibrary/book/urls.py
@@ -0,0 +1,7 @@
+# from django.conf.urls import url
+#
+# app_name = "library"
+#
+# urlpatterns = [
+#     url(r'^$',view=),
+# ]
